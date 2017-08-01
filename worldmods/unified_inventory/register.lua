9bd903b3c48fb6256c411aadeaeab8a3be6f054e
elite <cessna151@hotmail.com>
2017-07-31 20:04:35 -0400

    [unified_inventory] disable day/night buttons

--- worldmods/unified_inventory/register.lua
+++ worldmods/unified_inventory/register.lua
@@ -89,6 +89,8 @@ unified_inventory.register_button("home_gui_go", {
 	end,
 })
 
+-- Not needed
+--[[
 unified_inventory.register_button("misc_set_day", {
 	type = "image",
 	image = "ui_sun_icon.png",
@@ -136,6 +138,7 @@ unified_inventory.register_button("misc_set_night", {
 		return minetest.check_player_privs(player:get_player_name(), {settime=true})
 	end,
 })
+--]]
 
 unified_inventory.register_button("clear_inv", {
 	type = "image",
