c629c8f987e0a19b14d2c2c300a5f10b29a50840
Tre <ThomasMonroe@protonmail.com>
2017-08-07 12:15:16 -0500

    [death_pos] add bone finder
    * [death_pos] add bone finder
    
    * [death_pos] invert bone textures
    
    * [death_pos] update
    
    * [death_pos] update craft recipe

--- mods/death_pos/init.lua
+++ mods/death_pos/init.lua
@@ -59,3 +59,55 @@ func = function(name, param)
 	hud_update(minetest.get_player_by_name(name))
 end,
 })
+
+local find_bones = function(player, player_pos, pos)
+	local name = player:get_player_name()
+	if vector.equals(player:get_pos(),player_pos) then
+		player:setpos(pos)
+		minetest.chat_send_player(name, "Teleported to your last known death position.")
+	else
+		minetest.chat_send_player(name, "You must remain still for 5 seconds.")
+	end
+end
+
+minetest.register_node("death_pos:bone_finder", {
+	description = "Bone Finder",
+	tiles = {
+		"death_pos_bones_top.png^death_pos_magnifying_glass.png",
+		"death_pos_bones_bottom.png^death_pos_magnifying_glass.png",
+		"death_pos_bones_side.png^death_pos_magnifying_glass.png",
+		"death_pos_bones_side.png^death_pos_magnifying_glass.png",
+		"death_pos_bones_rear.png^death_pos_magnifying_glass.png",
+		"death_pos_bones_front.png^death_pos_magnifying_glass.png"
+	},
+	paramtype2 = "facedir",
+	groups = {oddly_breakable_by_hand = 2},
+	after_place_node = function(pos, placer, itemstack)
+		local meta = minetest.get_meta(pos)
+		local owner = placer:get_player_name()
+		meta:set_string("infotext","Right Click or double tap on me to teleport to your last known death position.\n\nPunch to toggle your last death position on the HUD.")
+	end,
+	on_rightclick = function(pos, node, clicker, itemstack)
+		local name = clicker:get_player_name()
+		local pos = minetest.string_to_pos(mod_storage:get_string(name))
+		pos.y = pos.y+1
+		local player_pos = clicker:get_pos()
+		minetest.chat_send_player(name, "Teleporting to bones, don't move for 5 seconds.")
+		minetest.after(5, find_bones, clicker, player_pos, pos)
+	end,
+	on_punch = function(pos, node, puncher)
+		local name = puncher:get_player_name()
+		if hud_on[name] == false then
+			hud_on[name] = true
+		elseif hud_on[name] == true then
+			hud_on[name] = false
+		end
+		hud_update(puncher)		
+	end
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "death_pos:bone_finder",
+	recipe = {"default:mese_block","bones:bones"},
+})
